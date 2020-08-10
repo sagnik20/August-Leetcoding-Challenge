ce5126754bf9e5b389be88a57517be71da515f98
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2020-08-10 12:18:49 +0530

    Create Day-08-PathSumIII.java

---
+++ Day-08-PathSumIII.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) {
+			return 0;
+		}
+		helper (root, sum);
+		pathSum (root.left, sum);
+		pathSum (root.right, sum);
+		return paths;
+    }
+    int paths = 0;
+	
+	private void helper (TreeNode root, int sum) {
+		
+		if (root != null && root.val == sum) ++paths;
+		if (root != null) {
+			sum -= root.val;
+			helper (root.left, sum);
+			helper (root.right, sum);
+		}
+	}
+}
