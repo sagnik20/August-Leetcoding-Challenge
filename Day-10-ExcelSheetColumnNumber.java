302170d9a48efa46799575dbb848565b885f86e0
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2020-08-12 09:46:10 +0530

    Create Day-10-ExcelSheetColumnNumber.java

---
+++ Day-10-ExcelSheetColumnNumber.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int titleToNumber(String s) {
+        int sum =0;
+        for(char c: s.toCharArray())
+            sum = sum *26 + (c - 'A') +1;
+        return sum;
+    }
+}
